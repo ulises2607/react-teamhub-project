519dcaa9ffb8a0adb9aadebb145449fe8cbafb71
Ulises <ulisescappa@gmail.com>
2024-07-25 22:39:57 -0300

    Se agrego tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <main>Main component</main>;
+  return (
+    <main className="w-[100%] h-[50vh] m-5 bg-[#680a0a] flex justify-center items-center ">
+      Main component
+    </main>
+  );
 }
 
 export default App;
