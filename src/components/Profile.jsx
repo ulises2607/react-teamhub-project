88a1bcfad7e38886d7d420fa41db31a9a53082e9
Rodrigo <rodrigo.e.gonza@gmail.com>
2024-08-11 22:16:04 -0300

    Se cambia imagen de perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -39,7 +39,7 @@ const Profile = () => {
       <button onClick={toggleMenu} className="flex items-center">
         <img
           className="w-10 h-10 rounded-full"
-          src={profile.profile_image || "/user.png"}
+          src={profile.profile_image || "https://cdn.discordapp.com/embed/avatars/1.png"}
           alt="User"
         />
         <span className="ml-2 text-white">
